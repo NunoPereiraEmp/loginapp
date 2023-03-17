96c74f058a9d58dea07ffe4e17b179429223225e
NunoPereiraEmp <nuno.pereira@valeriushub.com>
2023-03-17 17:53:46 +0000

    fixes

--- src/app/user/user-data/user-data.component.ts
+++ src/app/user/user-data/user-data.component.ts
@@ -11,28 +11,20 @@ import { MatTableDataSource } from '@angular/material/table';
 export class UserDataComponent implements OnInit {
   currentUser!: User;
   dataSource = new MatTableDataSource<User>();
-  displayedColumns: string[] = ['id_user', 'email', 'username', 'id_brand', 'active','1', '2', '3', '4', '5', '6', '7', '8', '14', '15', '17'];
-  displayColumnPer: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '14', '15', '17'];
+  displayedColumns: string[] = ['id_user', 'email', 'username', 'id_brand', 'active', '1', '2', '3', '4', '5', '6', '7', '8', '14', '15', '17'];
   searchValue: string = "";
   filteredColumns: string[] = [];
-  newColumnFilter: string[]=[];
+  newColumnFilter: string[] = [];
   currentUserAux!: User;
-
-
-  searchTerm: string = '';
-  filterCountries(searchTerm: any) {
-
-  }
 
   constructor(private userService: UserDataService) { }
 
   ngOnInit(): void {
     this.currentUser = this.userService.getCurrentUser();
-    console.log(this.currentUser);
     this.dataSource.data = [this.currentUser];
-    this.filteredColumns =  [...this.displayedColumns.slice()];
+    this.filteredColumns = [...this.displayedColumns.slice()];
     this.newColumnFilter = [...this.displayedColumns.slice()];
-    this.currentUserAux= { ...this.currentUser };
+    this.currentUserAux = { ...this.currentUser };
   }
 
   sendMessage(data: any) {
@@ -43,26 +35,24 @@ export class UserDataComponent implements OnInit {
     if (typeof data === 'undefined' || data === null) {
       return "Empty"
     }
-    
-    if(typeof data === 'string') {
+
+    if (typeof data === 'string') {
       return data;
     }
     return data;
 
   }
 
-  manageTableNamesError2(data: any, index:number) {
-    if(!isNaN(data) && typeof this.currentUserAux.permissions!= 'undefined') {
-     
-      return this.currentUserAux.permissions[index-5].description;
+  manageTableNamesError2(data: any, index: number) {
+    if (!isNaN(data) && typeof this.currentUserAux.permissions != 'undefined') {
+
+      return this.currentUserAux.permissions[index - 5].description;
     }
 
-    if (typeof  this.currentUserAux[data] === 'undefined' ||  this.currentUserAux[data] === null) {
+    if (typeof this.currentUserAux[data] === 'undefined' || this.currentUserAux[data] === null) {
       return "Empty"
     }
-    
-    const user= this.currentUser[data];
-    if(typeof data === 'string') {
+    if (typeof data === 'string') {
       return this.currentUserAux[data];;
     }
     return this.currentUserAux[data];
@@ -73,11 +63,5 @@ export class UserDataComponent implements OnInit {
     this.filteredColumns = this.newColumnFilter.filter(values => values.includes(this.searchValue));
     //this.currentUser.permissions=  this.newColumnFilter.filter(values => values.includes(this.searchValue)).slice();
   }
-
-  getProp(data: any) {
-    const aasda = "";
-  }
-
-
 
 }
